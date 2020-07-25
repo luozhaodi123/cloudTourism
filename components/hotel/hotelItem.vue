b61bf3e3dcd51085e078f5f0cc8adddbf22e76d2
zhoudongxiong <13534910699@163.com>
2020-07-25 21:45:56 +0800

    酒店item

---
+++ components/hotel/hotelItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="object">
+    <div class="item">
+      <div class="img">
+        <img src="#" alt />
+      </div>
+      <div class="info"></div>
+      <div class="price"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.object {
+  .item {
+    display: flex;
+    height: 266px;
+    padding: 26px 0;
+    border-bottom: 1px solid #e4e4e4;
+    .img {
+      width: 340px;
+    }
+    .info {
+      flex: 1;
+    }
+    .price {
+      width: 258px;
+    }
+  }
+}
+</style>
