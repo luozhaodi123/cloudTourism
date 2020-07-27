18acf0415f7d4ea6e9bc793f0a6cd4f7708bebdb
zhoudongxiong <13534910699@163.com>
2020-07-27 11:44:57 +0800

    地图右边部分完成

--- components/hotel/search.vue
+++ components/hotel/search.vue
@@ -54,7 +54,49 @@
       <div class="searchBtn" @click="conditionSearch">查看价格</div>
     </div>
     <div class="site">
-      <div class="info"></div>
+      <div class="info">
+        <div class="area" :class="{moreBtn:moreFlage}">
+          <div class="title">区域：</div>
+          <div class="site" ref="site">
+            <span v-for="(item,index) in hotelArea" :key="index">{{item}}&nbsp;&nbsp;</span>
+          </div>
+        </div>
+        <div class="more" @click="moreFlage=!moreFlage">
+          <span
+            class="iconfont"
+            :class="{iconjiantou_xia:moreFlage,
+             iconjiantou_shang:!moreFlage
+            }"
+          ></span>
+          等{{hotelArea.length}}个区域
+        </div>
+        <div class="price">
+          <div class="title">均价：</div>
+          <div class="span">
+            <div>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              ￥332
+            </div>
+            <div>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              ￥562
+            </div>
+            <div>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              <span class="iconfont iconhuangguan"></span>
+              ￥762
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="map" id="container"></div>
     </div>
     <div class="filterHotel">
@@ -126,6 +168,7 @@ export default {
     return {
       cityList: [],
       hotelInfo: [],
+      hotelArea: [],
       form: {
         price: null,
         city: null,
@@ -155,7 +198,10 @@ export default {
         ],
         enterTime: null,
         leftTime: null
-      }
+      },
+      // 控制区域查看更多
+      moreFlage: false
+      // 存放酒店所在区域
     };
   },
   methods: {
@@ -251,9 +297,37 @@ export default {
     //加载地图
     lodingMap() {
       var map = new AMap.Map("container");
+    },
+    // 获取区域地名
+    getArea() {
+      var Area = [];
+      this.$axios({
+        url: "hotels",
+        params: {
+          city: 197,
+          _limit: 100
+        }
+      }).then(res => {
+        this.hotelArea = res.data.data.map(item => {
+          return item.area;
+        });
+      });
     }
   },
-  mounted() {}
+  mounted() {
+    this.getHotel();
+    this.getArea();
+    window.onLoad = function() {
+      // 2. 地图库加载完毕的回调函数
+      var map = new AMap.Map("container");
+    };
+    var url =
+      "https://webapi.amap.com/maps?v=1.4.15&key=a0bb078664ce8d5d6fb67dae90e34bdd&callback=onLoad";
+    var jsapi = document.createElement("script");
+    jsapi.charset = "utf-8";
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
 };
 </script>
 
@@ -338,10 +412,65 @@ export default {
     display: flex;
     height: 230px;
     padding-bottom: 10px;
+    color: rgb(102, 102, 102);
     .info {
       flex: 1;
       padding-right: 20px;
-      background-color: orange;
+      // background-color: orange;
+      .area {
+        display: flex;
+        height: 46px;
+        width: 100%;
+        padding-right: 10px;
+        overflow: hidden;
+        .title {
+          width: 70px;
+          height: 20px;
+          line-height: 20px;
+          text-align: center;
+        }
+        .site {
+          display: flex;
+          flex: 1;
+          flex-wrap: wrap;
+          height: 46px;
+
+          padding-bottom: 0;
+          span {
+            height: 20px;
+            line-height: 20px;
+          }
+        }
+        &.moreBtn {
+          height: 120px;
+        }
+      }
+      .more {
+        width: 110px;
+        margin-left: 70px;
+        margin-top: 10px;
+        .iconfont {
+          color: orange;
+        }
+      }
+      .price {
+        display: flex;
+        margin-top: 20px;
+        .title {
+          width: 70px;
+          text-align: center;
+        }
+        .span {
+          display: flex;
+          flex: 1;
+          div {
+            margin-right: 20px;
+            .iconfont {
+              color: orange;
+            }
+          }
+        }
+      }
     }
     .map {
       width: 420px;
